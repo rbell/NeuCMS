5851bc975474f4c379b35dd516125bce37ad6920
Randy Bell <rbell+specexpress@irunon.net>
2012-04-10 18:30:39 -0600

    Added AssetController.cs

---
+++ NeuCMS/Controllers/AssetController.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using NeuCMS.Core.Entities;
+using NeuCMS.Repositories.EntityFramework;
+
+namespace NeuCMS.Controllers
+{
+    public class AssetController : Controller
+    {
+
+        public ActionResult Index(int id)
+        {
+            using (var repository = new ContentRepository() )
+            {
+                var asset = (from content in repository.Contents.OfType<MediaContent>()
+                                    where content.Id == id
+                                    select content.DigitalAsset).FirstOrDefault();
+                return base.File(asset.Data, asset.ContentType);
+            }
+        }
+
+    }
+}
