64aced94811abe2467362ccc96a34396dd8f940b
Randy Bell <rbell+specexpress@irunon.net>
2012-04-23 14:38:33 -0600

    Updated JQuery, Modernizr, and added JQGrid

---
+++ NeuCMS/Controllers/MetaDataConfigurationController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NeuCMS.Controllers
+{
+    public class MetaDataConfigurationController : Controller
+    {
+        //
+        // GET: /MetaDataDefinition/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ NeuCMS/Controllers/ViewConfigurationController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NeuCMS.Controllers
+{
+    public class ViewConfigurationController : Controller
+    {
+        //
+        // GET: /Pages/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
