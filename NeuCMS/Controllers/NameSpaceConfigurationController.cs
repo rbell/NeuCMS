990f44c6c59e1af70e1956ceebc4dcd66ffbbcda
Randy Bell <rbell+specexpress@irunon.net>
2012-04-26 06:48:01 -0600

    Started on jqgrid on namespaces view.

--- NeuCMS/Controllers/NameSpaceConfigurationController.cs
+++ NeuCMS/Controllers/NameSpaceConfigurationController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Lib.Web.Mvc.JQuery.JqGrid;
+using NeuCMS.Models;
+using NeuCMS.Repositories.EntityFramework;
 
 namespace NeuCMS.Controllers
 {
@@ -16,5 +19,35 @@ namespace NeuCMS.Controllers
             return View();
         }
 
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult NameSpaces(JqGridRequest request)
+        {
+            using (var db = new ContentRepository())
+            {
+                var nameSpaceQry = from n in db.NameSpaces orderby n.Id select n;
+
+                var totalRecs = db.NameSpaces.Count();
+                var response = new JqGridResponse()
+                                   {
+                                       TotalPagesCount =
+                                           (int) Math.Ceiling((float) totalRecs/(float) request.RecordsCount),
+                                       PageIndex = request.PageIndex,
+                                       TotalRecordsCount = totalRecs
+                                   };
+
+                var nameSpaces = nameSpaceQry
+                    .Skip(request.PageIndex*request.RecordsCount)
+                    .Take((request.PagesCount.HasValue ? request.PagesCount.Value : 1)*request.RecordsCount).ToList();
+
+                var jqGridRecords = from n in nameSpaces
+                        select new JqGridRecord<NameSpaceGridModel>(
+                            n.Id.ToString(),
+                            new NameSpaceGridModel(n));
+
+                response.Records.AddRange(jqGridRecords);
+
+                return new JqGridJsonResult() { Data = response };
+            }
+        }
     }
 }
