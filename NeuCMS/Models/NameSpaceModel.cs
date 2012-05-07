c0a1ae6b740ecae2e99b1f6c08f2a2e58d065552
Randy Bell <rbell+specexpress@irunon.net>
2012-05-07 17:32:50 -0600

    NameSpace JQGrid

---
+++ NeuCMS/Models/NameSpaceModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using NeuCMS.Core.Entities;
+
+namespace NeuCMS.Models
+{
+    public class NameSpaceGridModel
+    {
+        public NameSpaceGridModel(NameSpace ns)
+        {
+            Id = ns.Id;
+            NameSpaceName = ns.NameSpaceName;
+            Description = ns.Description;
+        }
+
+        public int Id { get; set; }
+        [DisplayName("Name Space")]
+        public string NameSpaceName { get; set; }
+        public string Description { get; set; }
+    }
+}
