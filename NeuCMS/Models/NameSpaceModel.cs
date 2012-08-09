ec64c04dbebc8c6e65cfab0408aa58804b18492e
Randy Bell <rbell+specexpress@irunon.net>
2012-08-09 16:05:47 -0600

    Changed Default column widths for NameSpaces

--- NeuCMS/Models/NameSpaceModel.cs
+++ NeuCMS/Models/NameSpaceModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using Lib.Web.Mvc.JQuery.JqGrid.DataAnnotations;
 using NeuCMS.Core.Entities;
 
 namespace NeuCMS.Models
@@ -16,9 +17,14 @@ namespace NeuCMS.Models
             Description = ns.Description;
         }
 
+        [JqGridColumnLayout(Width = 40)]
         public int Id { get; set; }
+
         [DisplayName("Name Space")]
+        [JqGridColumnLayout(Width = 200)]
         public string NameSpaceName { get; set; }
+
+        [JqGridColumnLayout(Width = 300)]
         public string Description { get; set; }
     }
 }
